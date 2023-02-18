49b39195dcb6c8fd666eb4da3591d320b0c768c2
01eh <prostre1@ukr.net>
2023-02-18 05:14:34 +0200

    v1.0.0

--- freedom_of_speech/static/freedom_of_speech/assets/js/main.js
+++ freedom_of_speech/static/freedom_of_speech/assets/js/main.js
@@ -235,7 +235,7 @@ function countUpFromTime(countFrom, id) {
 
     days = Math.floor(timeDifference / (secondsInADay) * 1);
     years = Math.floor(days / 365);
-    if (years > 1){ days = days - (years * 365) }
+    if (years > 0){ days = days - (years * 365) }
     hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
     mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
     secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
