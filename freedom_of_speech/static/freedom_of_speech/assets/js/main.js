4e4285b3625ebdbdd5ca7ab8c19be4365d9ddc69
01eh <prostre1@ukr.net>
2023-02-20 08:40:41 +0200

    v1.2.0

--- freedom_of_speech/static/freedom_of_speech/assets/js/main.js
+++ freedom_of_speech/static/freedom_of_speech/assets/js/main.js
@@ -326,7 +326,6 @@ $('#laws_button').on('click', function(e) {
         success: function(data, status, jqXHR) {
             // location.reload();
             $('#laws_text_span').text(data);
-            console.log(data)
 
             lawsMessage.removeClass('color-red')
             lawsMessage.addClass('color-green')
