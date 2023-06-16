0c91cdcacee54d6bdd834e7a0dfab38288fbbf02
01eh <prostre1@ukr.net>
2023-06-17 00:42:11 +0300

    v1.4.0

--- personal_site/wsgi.py
+++ personal_site/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_site.settings")
 
-# from jobs import updater
-# updater.start()
+from jobs import updater
+updater.start()
 
 application = get_wsgi_application()
