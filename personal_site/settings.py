61ceb55d095dfad368f591ebf93ce44fdef1d466
01eh <prostre1@ukr.net>
2023-08-02 10:02:45 +0300

    v1.5.0

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "django.middleware.gzip.GZipMiddleware",
 ]
 
 # CRON_CLASSES = [
